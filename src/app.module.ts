0665fe369bc53f5e5949ce8ccc06d391f09e8234
Prabakar S <prabhacoder@gmail.com>
2022-12-09 12:37:43 +0530

    morgon and helmet implemented, deprecated changes updated

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,8 +6,6 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
-import { User } from './users/user.entity';
-import { Report } from './reports/report.entity';
 const cookieSession = require('cookie-session');
 const settings = require('../ormconfig.js');
 
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
+import * as morgan from 'morgan';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   (app as any).set('etag', false);
+  app.use(helmet());
+  app.use(morgan('combined'));
   app.use((req, res, next) => {
     res.removeHeader('x-powered-by');
     res.removeHeader('date');
